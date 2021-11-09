c342e0a3de39245eb3996359d6fb6caa7e161e27
Venzard <assasin0084@gmail.com>
2021-11-09 11:58:14 +0200

    Login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,57 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes } from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+
+
+const routes: Routes=[
+  {
+    path:'',
+    redirectTo:'main',
+    pathMatch:'full'
+
+  },
+  {
+    path:'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+
+  },
+  {
+    path:'register',
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterModule),
+
+  },
+  {
+    path:'main',
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule),
+
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
