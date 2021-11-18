2e440965fad0b4090ae156045fe3de61dda83f56
Venzard <assasin0084@gmail.com>
2021-11-18 12:48:36 +0200

    Login/Register/Profile/Order/FinishOrder/UserNav fix module

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -3,10 +3,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {HttpService} from "../http.service";
 import {FormBuilder, Validators} from "@angular/forms";
 import {RepairFinishForm} from "./repair-finish-form";
-<<<<<<< HEAD
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
-=======
->>>>>>> origin/main
 
 @Component({
   selector: 'app-modal',
@@ -15,21 +12,15 @@ import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition}
 })
 export class ModalComponent {
   RepairFinishForm;
-<<<<<<< HEAD
   horizontalPosition: MatSnackBarHorizontalPosition = 'right';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
 
   constructor(public httpService: HttpService, private formBuilder: FormBuilder,public dialogRef:MatDialogRef<ModalComponent>, @Inject(MAT_DIALOG_DATA) public data:any,private _snackBar: MatSnackBar) {
-=======
-
-  constructor(public httpService: HttpService, private formBuilder: FormBuilder,public dialogRef:MatDialogRef<ModalComponent>, @Inject(MAT_DIALOG_DATA) public data:any) {
->>>>>>> origin/main
     this.RepairFinishForm = this.formBuilder.group({
       price: ['',[Validators.required]],
       cause: ['',[Validators.required]]
     });
   }
-<<<<<<< HEAD
   openSnackBar(message: string) {
     this._snackBar.open(message,'',{
       horizontalPosition: this.horizontalPosition,
@@ -43,15 +34,6 @@ export class ModalComponent {
 
       this.openSnackBar('Form not valid')
     } else {
-=======
-
-
-  onSubmit() {
-    if (this.RepairFinishForm.valid == false) {
-      console.log('Form not valid')
-    } else {
-      console.log(this.data.id);
->>>>>>> origin/main
       const body: RepairFinishForm = {
         id:this.data.id,
         price: this.RepairFinishForm.value.price,
@@ -60,20 +42,12 @@ export class ModalComponent {
       this.httpService.repairOrderFinishSubmit(body).subscribe(res => {
         if (res) {
 
-<<<<<<< HEAD
-=======
-          console.log(res);
->>>>>>> origin/main
           this.data.Done();
           this.closeModal()
 
         }
       }, error => {
-<<<<<<< HEAD
         this.openSnackBar(error.error.error)
-=======
-        console.log(error);
->>>>>>> origin/main
       });
     }
   }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -8,6 +8,13 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {AuthGuard} from "../auth/auth.guard";
 import {MatTableModule} from "@angular/material/table";
+import {MatDialogModule} from '@angular/material/dialog';
+import {ModalComponent} from "../modal/modal.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatInputModule} from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 
 const routes:Routes=[
@@ -18,7 +25,7 @@ const routes:Routes=[
   }
 ]
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent,ModalComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
@@ -26,7 +33,14 @@ const routes:Routes=[
     MatDividerModule,
     MatButtonModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSnackBarModule
   ]
 })
 export class ProfileModule { }
